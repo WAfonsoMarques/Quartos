66d4f6b0dc46253cb448ada8a1cf7021c96b80ee
Afonso Marques <afonsomar99@gmail.com>
2021-08-24 09:03:00 +0100

    Changed some prints

--- sorteio.py
+++ sorteio.py
@@ -1,12 +1,14 @@
 import time
 from random import randint
 
-clients = ["Afonso", "Antonio", "Guilherme", "Laura"]
-rooms = ["Quarto com mais janelas", "Quarto ao lado do quarto que tem mais janelas", "Quarto com varanda", "Quarto com candeeiro vermelho"]
 rooms_with_clients = {}
 
 
 def pick_rooms():
+    clients = ["Afonso", "Antonio", "Guilherme", "Laura"]
+    rooms = ["Quarto com mais janelas", "Quarto ao lado do quarto que tem mais janelas", "Quarto com varanda",
+             "Quarto com candeeiro vermelho"]
+
     for i in range(0, len(clients)):
         client = clients[randint(0, len(clients) - 1)]
         room = rooms[randint(0, len(rooms) - 1)]
@@ -20,12 +22,20 @@ def pick_rooms():
 def print_raffle():
     for name, room in rooms_with_clients.items():
         print(name.upper() + " ficou com: " + room)
-        time.sleep(0.5)
 
 
 if __name__ == '__main__':
+    print("________________Inicio do sorteio________________\n")
     for round in range(4):
+        rooms_with_clients.clear()
         pick_rooms()
         print_raffle()
-        print("\n________________NOVA RONDA________________")
+
+        if round == 2:
+            print("________________RONDA FINAL________________\n")
+            continue
+        elif round == 3:
+            print("________________FIM________________\n")
+            break
+        print("\n________________NOVA RONDA________________\n")
         time.sleep(1)
